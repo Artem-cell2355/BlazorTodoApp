138d07214db741d30dd863c26f3fe9b6d5ec605e
Artem-cell2355 <romanov.artem@stud.onu.edu.ua>
2026-03-18 02:02:20 +0200

    Додав завдання Advanced: JS Interop та Cascading Values

---
+++ wwwroot/app.js
@@ -0,0 +1,10 @@
+﻿// виклик JS з C#
+window.showAlert = (message) => {
+    alert("Повідомлення з C#: " + message);
+};
+
+// Виклик C# з JS [cite: 126]
+window.callDotNetMethod = async (dotNetHelper) => {
+    // викликаємо метод,він позначений [JSInvokable]
+    await dotNetHelper.invokeMethodAsync('GetMessageFromJS', 'Привіт від JavaScript!');
+};
